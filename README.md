1b3fb4816d7cce122451b3c8c57da965bbcce774
zzzzzzzs <1443539042@qq.com>
2023-02-12 16:42:08 +0800

    restructure

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 用来自己学习 rust 编写的 demo
 
 - HotKey ：编写的热键，主要是在特定作用域中操作快捷键。例如在 idea 中 ctrl+s 然后自动编译提高开发效率。
+
+# 创建项目
+```shell
+cargo new hello
+```
+
+每个项目分开了
---
+++ json/src/main.rs
@@ -0,0 +1,47 @@
+use json::object;
+
+fn main() {
+    let parsed = json::parse(r#"
+    {
+        "code": 200,
+        "success": true,
+        "payload": {
+            "features": [
+                "awesome",
+                "easyAPI",
+                "lowLearningCurve"
+            ]
+        }
+    }
+    "#).unwrap();
+
+    let instantiated = object! {
+        "code" => 200,
+        "success" => true,
+        "payload" => object!{
+            "features" => array![
+                "awesome",
+                "easyAPI",
+                "lowLearningCurve"
+            ]
+        }
+    };
+
+    assert_eq!(parsed, instantiated);
+    println!("解析输出字段：code={}", instantiated["code"]);
+    println!("解析输出字段：success={}", instantiated["success"]);
+    println!("解析输出对象：payload={:?}", instantiated["payload"]);
+    println!("解析输出数组：features={:?}", instantiated["payload"]["features"]);
+    println!("解析输出数组元素：0={}", instantiated["payload"]["features"][0]);
+    println!("解析输出数组元素：1={}", instantiated["payload"]["features"][1]);
+    println!("解析输出数组元素：2={}", instantiated["payload"]["features"][2]);
+}
+
+
+// 解析输出字段：code=200
+// 解析输出字段：success=true
+// 解析输出对象：payload=Object(Object { store: [("features", Array([Short("awesome"), Short("easyAPI"), Short("lowLearningCurve")]), 0, 0)] })
+// 解析输出数组：features=Array([Short("awesome"), Short("easyAPI"), Short("lowLearningCurve")])
+// 解析输出数组元素：0=awesome
+// 解析输出数组元素：1=easyAPI
+// 解析输出数组元素：2=lowLearningCurve
